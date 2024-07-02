5c3cf686e44dfe6f2128b0aed8c73cdefe2bcad7
refinedstructure <162787423+refinedstructure@users.noreply.github.com>
2024-07-03 00:57:03 +0530

    Add navigation links to the card
    Clicking a card opens the WIP card view now

--- Quotecards/Quotecards/ContentView.swift
+++ Quotecards/Quotecards/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
         self.tv_Shows = quoteCategories(name:"TV", description: "Quotes from your favorite TV shows", icon: "tv")
         self.movies = quoteCategories(name: "Movies", description: "Quotes from your favorite movies", icon: "popcorn")
         self.books = quoteCategories(name: "Books", description: "Quotes from your favorite books",icon: "book")
-        self.personalities = quoteCategories(name:"Personalities", description: "Quotes from famous people", icon: "person.2.wave.2.fill")
+        self.personalities = quoteCategories(name:"Personalities", description: "Quotes from famous people", icon: "person")
         self.categories = [tv_Shows, movies, personalities, books ]
     }
 
@@ -35,22 +35,27 @@ struct ContentView: View {
         
         NavigationStack {
             ScrollView{
-                LazyVGrid(columns: categoryColumns, spacing: 20) {
+                LazyVGrid(columns: categoryColumns, spacing: 15) {
                     ForEach(categories, id: \.id) { category in
-                        VStack{
-                            Text(category.name).font(.title).padding()
-                            Image(systemName:"\(category.icon)").resizable().frame(width:80, height:80).scaledToFit().padding()
-                            Text(category.description).font(.caption).padding()
-                                .foregroundStyle(.black.opacity(0.7))
-                        }
-                        .frame(width: 250, height: 250)
-                        .clipShape(.rect(cornerRadius: 20))
+                        NavigationLink{ QuoteView()
+                        }label:
+                        {
+                            VStack{
+                                Text(category.name).font(.title).padding()
+                                Image(systemName:"\(category.icon)").resizable().frame(width:80, height:80).scaledToFit().padding()
+                                Text(category.description).font(.caption).padding()
+                                    .foregroundStyle(.black.opacity(0.7))
+                            }
+                            .frame(width: 250, height: 250)
+                            .clipShape(.rect(cornerRadius: 20))
                             .overlay(
                                 RoundedRectangle(cornerRadius: 10).stroke()).frame(width:250, height:250)
+                                .foregroundStyle(.black)
+                        }
                     }
-               
                 }
                 .padding(.vertical)
+                .frame(maxWidth: .infinity)
                 .background(.ultraThickMaterial)
                 
             }
---
+++ Quotecards/Quotecards/QuoteView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteView.swift
+//  Quotecards
+//
+//  Created by Abhishek Rane on 03/07/24.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+}
