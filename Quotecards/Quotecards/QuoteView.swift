cabc61c3e1bcfc33e910ac5c2cf4c211b0ba89da
refinedstructure <162787423+refinedstructure@users.noreply.github.com>
2024-07-03 01:44:55 +0530

    Preview working no
    fixed preview

--- Quotecards/Quotecards/QuoteView.swift
+++ Quotecards/Quotecards/QuoteView.swift
@@ -9,22 +9,16 @@ import SwiftUI
 
 let decoder = JSONDecoder()
 let file = "quotes.json"
+//    let quotes: Quote
 
 struct QuoteView: View {
-    let quotes: Quote
     let selectedCategory: String
     
     var body: some View {
         Text(selectedCategory)
-        
     }
 }
 
 #Preview {
-    
-    let quotes: Quote = Bundle.main.decode("quotes.json")
-    
-    return QuoteView(quotes: quotes[0]!)
-    
-    QuoteView(selectedCategory: "Test")
+    QuoteView( selectedCategory: "Test")
 }
