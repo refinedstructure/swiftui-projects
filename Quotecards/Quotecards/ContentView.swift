971c4e8df95c3ed7354ced318485dd7e7fc31387
refinedstructure <162787423+refinedstructure@users.noreply.github.com>
2024-07-02 14:26:32 +0530

    Added Skeleton View
    Added Skeleton view for the main screen

--- Quotecards/Quotecards/ContentView.swift
+++ Quotecards/Quotecards/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    var tv_Shows: quoteCategories
+        var movies: quoteCategories
+        var books: quoteCategories
+        var personalities: quoteCategories
+        var categories: [quoteCategories]
+    
+    init(){
+        self.tv_Shows = quoteCategories(name:"TV", description: "Quotes from your favorite TV shows", icon: "tv")
+        self.movies = quoteCategories(name: "Movies", description: "Quotes from your favorite movies", icon: "popcorn")
+        self.books = quoteCategories(name: "Books", description: "Quotes from your favorite books",icon: "book")
+        self.personalities = quoteCategories(name:"Personalities", description: "Quotes from famous people", icon: "person")
+        self.categories = [tv_Shows, movies, books, personalities]
+    }
+
+    
+    let categoryColumns = [
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        NavigationStack {
+            ScrollView{
+                LazyVGrid(columns: categoryColumns, spacing: 50) {
+                    ForEach(categories, id: \.id) { category in
+                        VStack{
+                            Text(category.name).font(.title)
+                            Image(systemName:"\(category.icon)").resizable().frame(width:100, height:100).scaledToFit().padding()
+                            Text(category.description).font(.caption).padding()
+                        }
+                        .clipShape(.rect(cornerRadius: 20))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10).stroke())
+                         
+                    }
+               
+                }
+            }
+    
         }
-        .padding()
+        .navigationTitle("Quotes")
     }
 }
 
---
+++ Quotecards/Quotecards/quoteCategories.swift
@@ -0,0 +1,18 @@
+//
+//  quoteCategories.swift
+//  Quotecards
+//
+//  Created by Abhishek Rane on 02/07/24.
+//
+
+import Foundation
+
+
+struct quoteCategories: Identifiable {
+    
+    let id = UUID()
+    let name: String
+    let description: String
+    let icon: String
+    
+}
