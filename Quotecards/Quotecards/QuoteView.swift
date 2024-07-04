280f30dfd1d88b6f699de1158ecaf582a20c4e1e
refinedstructure <162787423+refinedstructure@users.noreply.github.com>
2024-07-05 02:48:35 +0530

    Add functionality to decode quotes fromJSON
    Add functionality to decode quotes fromJSON

--- Quotecards/Quotecards/QuoteView.swift
+++ Quotecards/Quotecards/QuoteView.swift
@@ -7,18 +7,56 @@
 
 import SwiftUI
 
-let decoder = JSONDecoder()
-let file = "quotes.json"
+
+
+
+
+
+
+
 //    let quotes: Quote
 
 struct QuoteView: View {
     let selectedCategory: String
+    @State private var allQuotes: [Quote] = []
     
+
     var body: some View {
-        Text(selectedCategory)
+        ScrollView(.horizontal){
+            ForEach(allQuotes, id: \.id) { quote in
+                LazyHStack{
+                    Text(quote.quote)
+                }
+            }
+        }.onAppear{
+            allQuotes = decodeData()
+
+        }
     }
+    
+    func decodeData() -> [Quote]
+    {
+        guard let quotesJSONURL = Bundle.main.url(forResource: "quotes", withExtension: "json") else {
+            fatalError("Could not load quotes.json")
+        }
+
+        guard let quotesData = try? Data(contentsOf: quotesJSONURL) else {
+            fatalError("couldn't convert data")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let decodedQuotes = try? decoder.decode([Quote].self, from: quotesData) else {
+            fatalError("There was a problem decoding the data...")
+        }
+        return decodedQuotes
+    }
+    
 }
 
 #Preview {
-    QuoteView( selectedCategory: "Test")
+    QuoteView(
+        selectedCategory: "Test"
+        
+    )
 }
