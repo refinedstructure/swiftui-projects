bc4ef4dc4aa39a38335ea7042992930a1e3a8493
refinedstructure <162787423+refinedstructure@users.noreply.github.com>
2024-08-11 19:48:03 +0530

    Refactoring  BMICalc Phase 1
    Refactoring  BMICalc Phase 1 - organize code to split data and views

--- BMI Calculator/BMICalc/ContentView.swift
+++ BMI Calculator/BMICalc/ContentView.swift
@@ -7,47 +7,6 @@
 
 import SwiftUI
 
-
-struct bmiHistoryRecords:Identifiable, Codable{
-    var id = UUID()
-    let weightCaptured:Double
-    let heightCaptured:Double
-    let weightUnitsCaptured:String
-    let heightUnitsCaptured: String
-    let bmiCaptured: Double
-    let bmiClassCaptured: String
-    let bmiDateCaptured:Date
-}
-
-@Observable
-class bmiRecordItems {
-    var records = [bmiHistoryRecords]() {
-        didSet {
-            if let encoded = try?
-                JSONEncoder().encode(records) {
-                UserDefaults.standard.set(encoded,
-                                          forKey: "Records")
-            }
-        }
-    }
-    
-    init() {
-        if let savedRecords =
-            UserDefaults.standard.data(forKey: "Records") {
-            if let decodedItems = try?
-                JSONDecoder().decode(
-                    [bmiHistoryRecords].self, from:
-                        savedRecords) {
-                records = decodedItems
-                return
-            }
-        }
-}
-}
-
-
-
-
 struct ContentView: View {
     
     enum fieldEmptyError: Error{
