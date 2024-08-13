4b30920593385e25bcd47011b3e3d4512b3be2dd
refinedstructure <162787423+refinedstructure@users.noreply.github.com>
2024-08-13 21:03:50 +0530

    Updated Clear Button
    Updated Clear Button

--- BMI Calculator/BMICalc/ContentView.swift
+++ BMI Calculator/BMICalc/ContentView.swift
@@ -11,15 +11,16 @@ struct ContentView: View {
 
 //INSTANCE OF THE BMI CALCULATOR CLASS
 @State private var bmiCalc = bmiCalculator()
+    
     var body: some View{
         
         if bmiCalc.allDataEntered {
                 Text("Your BMI is \(bmiCalc.bmi) ")
                     .padding()
-                    .animation(.spring, value: bmiCalc.bmi)
+                    .animation(.smooth, value: 100)
                 Text("Your BMI Category is \(bmiCalc.bmiClass)")
                     .padding()
-                    .animation(.spring, value: bmiCalc.bmi)
+                    .animation(.smooth, value: 100)
         }
         else {
             Text("Calculate Your BMI")
@@ -27,42 +28,47 @@ struct ContentView: View {
                 .font(.title)
             Text("Start by entering your height and weight")
                 .padding()
-                .font(.caption)
         }
         
         VStack {
-            TextField("Height", text: $bmiCalc.inputHeight)
-                .textFieldStyle(.roundedBorder)
-                .disableAutocorrection(true)
-                .keyboardType(.decimalPad)
-            
-            Picker("Enter height units", selection: $bmiCalc.selectedHeightUnit)
-            {
-                ForEach(bmiCalc.heightUnits, id: \.self){
-                    Text($0)
-                }
-            }.pickerStyle(.segmented)
-            
-            TextField("Weight", text: $bmiCalc.inputWeight)
-                .textFieldStyle(.roundedBorder)
-                .disableAutocorrection(true)
-                .keyboardType(.decimalPad)
-            
-            Picker("Enter weight units", selection: $bmiCalc.selectedWeightUnit)
-            {
-                ForEach(bmiCalc.weightUnits, id: \.self){
-                    Text($0)
-                }
-            }.pickerStyle(.segmented)
-        }
+                TextField("Enter Height", text: $bmiCalc.inputHeight)
+                    .textFieldStyle(.roundedBorder)
+                    .disableAutocorrection(true)
+                    .keyboardType(.decimalPad)
+                
+                Picker("Enter height units", selection: $bmiCalc.selectedHeightUnit)
+                {
+                    ForEach(bmiCalc.heightUnits, id: \.self){
+                        Text($0)
+                    }
+                }.pickerStyle(.segmented)
+                
+                TextField("Enter Weight", text: $bmiCalc.inputWeight)
+                    .textFieldStyle(.roundedBorder)
+                    .disableAutocorrection(true)
+                    .keyboardType(.decimalPad)
+                
+                Picker("Enter weight units", selection: $bmiCalc.selectedWeightUnit)
+                {
+                    ForEach(bmiCalc.weightUnits, id: \.self){
+                        Text($0)
+                    }
+                }.pickerStyle(.segmented)
+            }
         .padding()
         
         HStack{
-            Button("Clear"){
+            Button{
                 bmiCalc.inputHeight = ""
                 bmiCalc.inputWeight = ""
             }
-            .buttonStyle(BorderlessButtonStyle())
+            label: {
+                Image(systemName:"xmark.circle")
+                Text("Clear")
+            }
+            .buttonStyle(BorderedButtonStyle())
+            .foregroundStyle(.black)
+
 //            Button("Save"){
 //                
 //            }
@@ -76,10 +82,3 @@ struct ContentView: View {
     ContentView()
 }
 
-
-//
-//BMI Categories:
-//Underweight = <18.5
-//Normal weight = 18.5–24.9
-//Overweight = 25–29.9
-//Obesity = BMI of 30 or greater
