35e4e3e45f54e858ce801dfe06b71f6fc882eead
refinedstructure <162787423+refinedstructure@users.noreply.github.com>
2024-08-13 16:40:39 +0530

    Refactor in progress
    Refactor in progress

--- BMI Calculator/BMICalc/ContentView.swift
+++ BMI Calculator/BMICalc/ContentView.swift
@@ -11,27 +11,20 @@ struct ContentView: View {
 //    INPUT GATHERED FROM USERS
 @State private var weightString = ""
 @State private var heightString = ""
-@State private var neckCircumfString = ""
-@State private var waistCircumfString = ""
 
+
+    
 //INSTANCE OF THE BMI CALCULATOR CLASS
 let bmiCalc = bmiCalculator()
 
-
     var body: some View{
-        
-        
+    
     TextField("Weight", text: $weightString)
             .textFieldStyle(.roundedBorder)
             .disableAutocorrection(true)
     TextField("Height", text: $heightString)
             .textFieldStyle(.roundedBorder)
             .disableAutocorrection(true)
-        
-    TextField("Neck circumference", text: $neckCircumfString)
-        .textFieldStyle(.roundedBorder)
-        .disableAutocorrection(true)
-    TextField("Waist Circumference", text: $waistCircumfString)
 
         
     }
