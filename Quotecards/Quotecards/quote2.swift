32993171c354fabff2c374a0560e5861ea1058ba
refinedstructure <162787423+refinedstructure@users.noreply.github.com>
2024-10-15 13:53:21 +0530

    Begin migration to swiftdata
    Begin migration to swiftdata

---
+++ Quotecards/Quotecards/quote2.swift
@@ -0,0 +1,24 @@
+//
+//  quote2.swift
+//  Quotecards
+//
+//  Created by Abhishek Rane on 15/10/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class quoteCard: Identifiable {
+    var id: String
+    var quoteText: String
+    var quoteCategory: String
+    var quoteAuthor: String
+    
+    init(){
+        id = UUID().uuidString
+        self.quoteText = ""
+        self.quoteCategory = ""
+        self.quoteAuthor = ""
+    }
+}
